9d0aabb1b29dd884ad7496dacd95638977445cde
bwarminski <bwarminski@localytics.com>
2018-07-09 12:29:42 -0400

    Boilerplace necessary for client generation

--- deployment/pkg/apis/brettdeployment/v1/types.go
+++ deployment/pkg/apis/brettdeployment/v1/types.go
@@ -22,24 +22,29 @@ type BrettDeployment struct {
 	meta_v1.ObjectMeta `json:"metadata,omitempty"`
 
 	// Spec is the custom resource spec
-	Spec MyResourceSpec `json:"spec"`
+	Spec DeploymentSpec `json:"spec"`
+	Status DeploymentStatus `json:"status"`
 }
 
-// MyResourceSpec is the spec for a MyResource resource
-type MyResourceSpec struct {
+type DeploymentSpec struct {
 	// Message and SomeValue are example custom spec fields
 	//
 	// this is where you would put your custom resource data
-	Message   string `json:"message"`
-	SomeValue *int32 `json:"someValue"`
+	Name   string `json:"name"`
+	Parent string `json:"parent"`
+	InitialValue string `json:"initialValue, omitempty"`
+}
+
+type DeploymentStatus struct {
+	Status string `json:"status"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // MyResourceList is a list of MyResource resources
-type MyResourceList struct {
+type BrettDeploymentList struct {
 	meta_v1.TypeMeta `json:",inline"`
 	meta_v1.ListMeta `json:"metadata"`
 
-	Items []MyResource `json:"items"`
+	Items []BrettDeployment `json:"items"`
 }
